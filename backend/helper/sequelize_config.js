65181f362f9de38bbe41e70ea187173b1ab3d0f3
mudasir-pro <mudasirabbasi26@gmail.com>
2024-02-18 20:39:02 +0500

    resolve error

--- backend/helper/sequelize_config.js
+++ backend/helper/sequelize_config.js
@@ -1,10 +1,6 @@
 import { Sequelize } from "sequelize";
 
-<<<<<<< HEAD
   export const sequelize = new Sequelize( "postgres",'postgres', 'postgres', {
-=======
-    export const sequelize = new Sequelize( "postgres",'postgres', 'postgres', {
->>>>>>> 110711e3c82c9159831fc42485f91e444bcef648
   host: 'localhost',
   dialect: 'postgres',
   logging:false
